49bd3a054857e0bed83f7205acb9f62c91d613dc
ttuan <tuantv.nhnd@gmail.com>
2025-09-14 22:34:41 +0700

    feat: Add Lambda IAM role and SQS policy for backend TF module

---
+++ terraform/envs/prd/6.backend/sqs.tf
@@ -0,0 +1,18 @@
+###################
+# SQS Screenshot Processing Queue
+###################
+
+# Main processing queue for screenshot generation jobs
+resource "aws_sqs_queue" "screenshot_processing" {
+  name = "${var.project}-${var.env}-processing-queue"
+
+  # Basic queue configuration
+  visibility_timeout_seconds = 300    # 5 minutes
+  message_retention_seconds  = 1209600 # 14 days
+
+  tags = {
+    Name    = "${var.project}-${var.env}-processing-queue"
+    Service = "screenshot-processing"
+  }
+}
+
